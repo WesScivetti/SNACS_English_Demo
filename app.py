971eb0b7a1b0a0586f8b196f214b9a83b09017a4
WesScivetti <wss37@georgetown.edu>
2025-06-03 21:33:06 -0400

    update

--- app.py
+++ app.py
@@ -2,22 +2,26 @@ import gradio as gr
 from transformers import pipeline
 
 # Load the pipeline (token classification)
-token_classifier = pipeline("token-classification", model="WesScivetti/SNACS_English", aggregation_strategy="simple")
+#token_classifier = pipeline("token-classification", model="WesScivetti/SNACS_English", aggregation_strategy="simple")
 
+
+@spaces.GPU  # <-- required for ZeroGPU
 def classify_tokens(text):
+    token_classifier = pipeline("token-classification", model="WesScivetti/SNACS_English",
+                                aggregation_strategy="simple")
+
     results = token_classifier(text)
     output = ""
     for entity in results:
         output += f"{entity['word']} ({entity['entity_group']}, score={entity['score']:.2f})\n"
     return output.strip()
 
-# Gradio Interface
 iface = gr.Interface(
     fn=classify_tokens,
-    inputs=gr.Textbox(lines=4, placeholder="Enter a sentence..."),
+    inputs=gr.Textbox(lines=4, placeholder="Enter text to be classified..."),
     outputs="text",
-    title="Token Classification with Transformers",
-    description="Named Entity Recognition (NER) using Hugging Face Transformers"
+    title="SNACS Tagging in English",
+    description="SNACS Tagging in English"
 )
 
 iface.launch()
